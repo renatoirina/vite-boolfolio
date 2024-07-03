cf981a75b6340454436c638a08e0d4a0e400f8b2
renatoirina <irinarenato793@gmail.com>
2024-07-03 15:23:10 +0200

    feat: test for first connection to API

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  created() {
+    axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
+      this.projects = resp.data.results;
+    })
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="row">
+      <div class="col-3" v-for="project in projects">
+        <div class="card">
+          <div class="card-body">
+            <h1> {{ project.title }}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
