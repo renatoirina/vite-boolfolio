f86c3e4b68794022fe93477b4e014908d1ab2944
renatoirina <irinarenato793@gmail.com>
2024-07-03 16:14:39 +0200

    feat: implemented the most important information on the cards

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <script>
-import axios from "axios"
+// Importa il modulo axios per effettuare le richieste HTTP
+import axios from "axios";
+
 export default {
   data() {
     return {
+      // Inizializza l'array projects vuoto
       projects: [],
-    }
+    };
   },
 
+  // Metodo eseguito al momento della creazione del componente
   created() {
+    // Effettua una richiesta GET all'API per ottenere i progetti
     axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
+      // Assegna i dati ricevuti alla variabile projects
       this.projects = resp.data.results;
-    })
+    });
   }
-}
+};
 </script>
 
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-3" v-for="project in projects">
+      <!-- Ciclo v-for per iterare su ogni progetto -->
+      <div class="col-3" v-for="project in projects" :key="project.id">
         <div class="card">
           <div class="card-body">
-            <h1> {{ project.title }}</h1>
+            <!-- Visualizzo il titolo del progetto -->
+            <h1>{{ project.title }}</h1>
+            <!-- Visualizzo la descrizione del progetto -->
+            <p>{{ project.description }}</p>
+            <!-- Visualizzo il nome del linguaggio del progetto -->
+            <p><strong>Language:</strong> {{ project.type.name }}</p>
+            <!-- Visualizzo il campo del progetto -->
+            <p><strong>Field:</strong> {{ project.type.field }}</p>
+            <!-- Sezione per visualizzare le tecnologie utilizzate nel progetto -->
+            <div>
+              <strong>Technologies:</strong>
+              <ul>
+                <!-- Ciclo v-for per iterare su ogni tecnologia associata al progetto -->
+                <li v-for="tech in project.technologies" :key="tech.id">
+                  {{ tech.name }}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -29,4 +53,9 @@ export default {
   </div>
 </template>
 
-<style></style>
+<style>
+/* Stile per aggiungere spazio tra le card */
+.card {
+  margin-bottom: 20px;
+}
+</style>
