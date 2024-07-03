2caa8131ce795ef322362db65008dad17b6ce6b1
renatoirina <irinarenato793@gmail.com>
2024-07-03 16:48:43 +0200

    feat: added the ProjectCard and I make the card a little beauty

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <script>
-// Importa il modulo axios per effettuare le richieste HTTP
+// Importo il modulo axios per effettuare richieste HTTP
 import axios from "axios";
+// Importo il nuovo componente ProjectCard
+import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
+  components: {
+    ProjectCard
+  },
   data() {
     return {
-      // Inizializza l'array projects vuoto
+      // Inizializzo l'array projects vuoto
       projects: [],
     };
   },
 
-  // Metodo eseguito al momento della creazione del componente
+  // Questo metodo viene eseguito al momento della creazione del componente
   created() {
-    // Effettua una richiesta GET all'API per ottenere i progetti
+
+    // Effettuo una richiesta GET all'API per ottenere i progetti
     axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
-      // Assegna i dati ricevuti alla variabile projects
+      
+      // Assegno i dati ricevuti alla variabile projects
       this.projects = resp.data.results;
     });
   }
@@ -22,40 +29,14 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container my-5">
     <div class="row">
-      <!-- Ciclo v-for per iterare su ogni progetto -->
-      <div class="col-3" v-for="project in projects" :key="project.id">
-        <div class="card">
-          <div class="card-body">
-            <!-- Visualizzo il titolo del progetto -->
-            <h1>{{ project.title }}</h1>
-            <!-- Visualizzo la descrizione del progetto -->
-            <p>{{ project.description }}</p>
-            <!-- Visualizzo il nome del linguaggio del progetto -->
-            <p><strong>Language:</strong> {{ project.type.name }}</p>
-            <!-- Visualizzo il campo del progetto -->
-            <p><strong>Field:</strong> {{ project.type.field }}</p>
-            <!-- Sezione per visualizzare le tecnologie utilizzate nel progetto -->
-            <div>
-              <strong>Technologies:</strong>
-              <ul>
-                <!-- Ciclo v-for per iterare su ogni tecnologia associata al progetto -->
-                <li v-for="tech in project.technologies" :key="tech.id">
-                  {{ tech.name }}
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!-- Uso il componente ProjectCard per iterare su ogni progetto -->
+      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
     </div>
   </div>
 </template>
 
 <style>
-/* Stile per aggiungere spazio tra le card */
-.card {
-  margin-bottom: 20px;
-}
+/* Puoi aggiungere ulteriori stili globali qui */
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100 shadow-sm">
+      <div class="card-body d-flex flex-column">
+        <!-- Visualizzo il titolo del progetto -->
+        <h5 class="card-title">{{ project.title }}</h5>
+
+        <!-- Visualizzo la descrizione del progetto -->
+        <p class="card-text">{{ project.description }}</p>
+
+        <!-- Visualizzo il nome del linguaggio del progetto -->
+        <p><strong>Language:</strong> {{ project.type.name }}</p>
+
+        <!-- Visualizzo il campo del progetto -->
+        <p><strong>Field:</strong> {{ project.type.field }}</p>
+
+        <!-- Sezione per visualizzare le tecnologie utilizzate nel progetto -->
+        <div class="mt-auto">
+          <strong>Technologies:</strong>
+          <ul class="list-inline">
+
+            <!-- Uso un ciclo v-for per iterare su ogni tecnologia associata al progetto -->
+            <li class="list-inline-item badge badge-primary" v-for="tech in project.technologies" :key="tech.id">
+              {{ tech.name }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+/* Stile per aggiungere spazio tra le card e renderle esteticamente migliori */
+.card {
+  margin-bottom: 20px;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.card-title {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.card-text {
+  flex-grow: 1;
+  font-size: 1rem;
+}
+
+.list-inline-item {
+  margin-right: 10px;
+}
+
+.badge-primary {
+  background-color: #007bff;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 20px;
+}
+</style>
